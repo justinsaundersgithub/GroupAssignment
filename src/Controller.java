e57ae0b70b254bdca2fb53572e7589533d0c4e6b
Max Henderson <maxwell.blair.henderson@gmail.com>
2019-06-07 13:36:05 -0700

    Added some more black methods to work with

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,57 @@
 public class Controller {
+    private String username;
+    private Model model;
+    private View user;
+
+    public void loginRequest(String username, String password){
+        if(model.verifyCredentials(username, password)){
+            model.attachUser(this);                         //The controller attaches itself to the user list in the model so that it can be notified of changes
+            user.switchWindow("landingPage");   //Tell the view to switch windows
+        }
+        else{
+            /*Todo
+            Throw an exception and output an error message to the user
+            in the view
+             */
+        }
+    }
+
+    public void registerRequest(String username, String email, String password){
+        if(model.registerUser(username,email,password)){
+            loginRequest(username,password);
+        }
+        else{
+            /*Todo
+            Throw some exception or handle the case that the username or account already exists.
+            Output something to the view to indicate that there is an error.
+             */
+        }
+    }
+
+    public void accountRecovery(String email){
+        try {
+            model.recoverPassword(email);
+            /*Todo
+            If this doesnt fail, we will need to have a window pop up that has
+            the users password displayed
+             */
+        }
+        catch (NoSuchAccountException e){
+            /*Todo
+            Call the view to display an error message
+             */
+        }
+    }
+    //This method will be called when the window is closed and the user exits the program
+    public void userExit(){
+        model.detachUser(this);
+    }
+
+    public void receiveMessage(String message){
+        /*Todo
+
+         */
+    }
+
+
 }
--- src/View.java
+++ src/View.java
@@ -1,2 +1,37 @@
 public class View {
+
+    public void updateMessagePane(String message){
+        /*Todo
+        This is the method that will be called by the controller when a new message is sent out and needs to be
+        displayed in the messages window
+         */
+    }
+
+    public void switchWindow(String windowName){
+        /*Todo
+        Controller will call this method to switch the view from one window
+        to another.
+         */
+    }
+
+    public void errorWindow(String message){
+        /*Todo
+
+         */
+    }
+
+    /*Todo
+    Will need a window for each view
+    -Login window
+    -Landing page
+    -Chatroom
+    -Registration
+    -Account Recovery
+
+     */
+
+    /*Todo
+    Very important, on exit, we need to detach the user from the model
+    Call controller.userExit()
+     */
 }
